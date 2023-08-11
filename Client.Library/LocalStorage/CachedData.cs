fb6be90dd5e57cf30e9b3343d5f64f74837e5885
Timothy Tandt <Timothy.tandt@outlook.be>
2023-08-11 16:18:09 +0200

    Implemented LocalStorage

---
+++ Client.Library/LocalStorage/CachedData.cs
@@ -0,0 +1,6 @@
+﻿namespace Client.Library.LocalStorage;
+public class CachedData<T>
+{
+    public T Data { get; set; }
+    public DateTimeOffset Expiration { get; set; }
+}
---
+++ Client.Library/LocalStorage/Interfaces/ILocalStorage.cs
@@ -0,0 +1,8 @@
+﻿namespace Client.Library.LocalStorage.Interfaces;
+
+public interface ILocalStorage
+{
+    Task<T> GetAsync<T>(string key);
+    Task RemoveAsync(string key);
+    Task SetAsync<T>(string key, T value, TimeSpan cacheValidity);
+}
