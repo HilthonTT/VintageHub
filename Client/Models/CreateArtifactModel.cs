aa3c2a648c4ba864c5ee229af551ba3411833636
Timothy Tandt <Timothy.tandt@outlook.be>
2023-08-13 20:25:59 +0200

    Implemented Create.razor page

---
+++ Client/Models/CreateArtifactModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VintageHub.Client.Models;
+
+public class CreateArtifactModel
+{
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string Name { get; set; }
+
+    [Required]
+    [StringLength(256)]
+    public string Description { get; set; }
+
+    public string ImageUrl { get; set; } = "";
+
+    [Required]
+    public int Quantity { get; set; }
+
+    [Required]
+    public decimal Price { get; set; }
+
+    [Required]
+    public int VendorId { get; set; }
+
+    [Required]
+    public int CategoryId { get; set; }
+
+    [Required]
+    public int EraId { get; set; }
+
+    [Required]
+    public bool Availability { get; set; }
+}
