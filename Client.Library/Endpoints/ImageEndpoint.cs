e886499557352eb8d11686528a7b2dbdbd6dd950
Timothy Tandt <Timothy.tandt@outlook.be>
2023-08-14 14:44:27 +0200

    Compressing the image in ImageEndpoint & updated Create.razor page.

--- Client.Library/Endpoints/ImageEndpoint.cs
+++ Client.Library/Endpoints/ImageEndpoint.cs
@@ -3,6 +3,7 @@ using Client.Library.LocalStorage.Interfaces;
 using Client.Library.Models.Cache;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using SixLabors.ImageSharp.Formats.Jpeg;
 
 namespace Client.Library.Endpoints;
 public class ImageEndpoint : IImageEndpoint
@@ -10,6 +11,7 @@ public class ImageEndpoint : IImageEndpoint
     private static readonly TimeSpan CacheTimeSpan = TimeSpan.FromMinutes(30);
     private const string CacheName = nameof(ImageEndpoint);
     private const string ApiEndpointUrl = "api/Image";
+    private const int MaxAllowedSize = int.MaxValue;
     private readonly HttpClient _httpClient;
     private readonly ILocalStorage _localStorage;
 
@@ -21,14 +23,35 @@ public class ImageEndpoint : IImageEndpoint
         _localStorage = localStorage;
     }
 
+    private static async Task<MultipartFormDataContent> CompressFileAsync(IBrowserFile imageFile)
+    {
+        using var imageStream = imageFile.OpenReadStream(MaxAllowedSize);
+
+        using var image = await Image.LoadAsync(imageStream);
+
+        var encoder = new JpegEncoder
+        {
+            Quality = 80,
+        };
+
+        using var compressedStream = new MemoryStream();
+        await image.SaveAsync(compressedStream, encoder);
+
+        compressedStream.Seek(0, SeekOrigin.Begin);
+
+        var content = new MultipartFormDataContent
+        {
+            { new StreamContent(imageFile.OpenReadStream(MaxAllowedSize)), "imageFile", imageFile.Name }
+        };
+
+        return content;
+    }
+
     public async Task<string> UploadImageAsync(IBrowserFile imageFile)
     {
         try
         {
-            using var content = new MultipartFormDataContent
-            {
-                { new StreamContent(imageFile.OpenReadStream()), "image", imageFile.Name }
-            };
+            using var content = await CompressFileAsync(imageFile);
 
             using var response = await _httpClient.PostAsync(ApiEndpointUrl, content);
             response.EnsureSuccessStatusCode();
