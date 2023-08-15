47a8b9b973d77eca7035454d18dc6cc62ebb3544
Timothy Tandt <Timothy.tandt@outlook.be>
2023-08-15 20:44:55 +0200

    Modification with Create Pages.
    - Moved them into Admin page
    - Added /Admin to their URL
    - Added error message for data annotation
    - Implemented CreateVendor page with Create Model.

--- Client/Models/CreateArtifactModel.cs
+++ Client/Models/CreateArtifactModel.cs
@@ -4,33 +4,34 @@ namespace VintageHub.Client.Models;
 
 public class CreateArtifactModel
 {
-    public int Id { get; set; }
 
-    [Required]
-    [StringLength(50)]
+    [Required(ErrorMessage = "Please provide the artifact's name.")]
+    [StringLength(50, ErrorMessage = "The artifact's name must not exceed 50 characters.")]
     public string Name { get; set; }
 
-    [Required]
-    [StringLength(256)]
+    [Required(ErrorMessage = "Please provide the artifact's description.")]
+    [StringLength(256, ErrorMessage = "The artifact's description must not exceed 256 characters.")]
     public string Description { get; set; }
 
+    [Display(Name = "Image URL")]
     public string ImageUrl { get; set; } = "";
 
-    [Required]
+    [Required(ErrorMessage = "Please provide the artifact's quantity.")]
     public int Quantity { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Please provide the artifact's price.")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0.")]
     public decimal Price { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Please provide the vendor ID.")]
     public int VendorId { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Please provide the category ID.")]
     public int CategoryId { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Please provide the era ID.")]
     public int EraId { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Please specify availability.")]
     public bool Availability { get; set; }
 }
---
+++ Client/Models/CreateVendorModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VintageHub.Client.Models;
+
+public class CreateVendorModel
+{
+    [Range(1, int.MaxValue, ErrorMessage = "Please provide a value user for the owner.")]
+    public int OwnerUserId { get; set; }
+
+    [Required(ErrorMessage = "Please provide the vendor's name.")]
+    [StringLength(50, ErrorMessage = "The vendor's name must not be above 50 characters.")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Please provide the vendor's description.")]
+    [StringLength(256, ErrorMessage = "The vendor's descritpion must not be above 256 characters")]
+    public string Description { get; set; }
+
+    [Required]
+    [Display(Name = "Date Founded")]
+    public DateTime DateFounded { get; set; }
+}
