808e320ca742a8215460429d8fa28578339ad17a
Timothy Tandt <Timothy.tandt@outlook.be>
2023-08-18 18:05:05 +0200

    Implemented OrderDetails.razor

---
+++ Client/Models/UpdateOrderModel.cs
@@ -0,0 +1,20 @@
+﻿using Client.Library.Models;
+
+namespace VintageHub.Client.Models;
+
+public class UpdateOrderModel
+{
+    public bool IsComplete { get; set; }
+    public bool IsCanceled { get; set; }
+
+    public UpdateOrderModel()
+    {
+        
+    }
+
+    public UpdateOrderModel(OrderModel order)
+    {
+        IsComplete = order.IsComplete;
+        IsCanceled = order.IsCanceled;
+    }
+}
