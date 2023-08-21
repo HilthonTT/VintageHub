42e2eda14b88a71f360317e1487b4e55d5a39129
Timothy Tandt <Timothy.tandt@outlook.be>
2023-08-21 17:33:08 +0200

    OrderDetails HTML Overhaul part 1

--- Client/Models/Data/OrderDetailsDataModel.cs
+++ Client/Models/Data/OrderDetailsDataModel.cs
@@ -4,6 +4,17 @@ namespace VintageHub.Client.Models.Data;
 
 public class OrderDetailsDataModel
 {
+    public OrderDetailsDataModel(OrderDetailsModel orderDetails)
+    {
+        OrderDetails = orderDetails;
+    }
+
+    public OrderDetailsDataModel()
+    {
+        
+    }
+
     public OrderDetailsModel OrderDetails { get; set; }
     public ArtifactModel Artifact { get; set; }
+    public decimal TotalPrice { get; set; }
 }
