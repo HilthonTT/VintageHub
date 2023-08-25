afba3545ef86bc14797fd0f791d36e0342c99f1f
Timothy Tandt <Timothy.tandt@outlook.be>
2023-08-25 12:42:29 +0200

    Modified ArtifactModel in Client.

--- Client.Library/Models/ArtifactModel.cs
+++ Client.Library/Models/ArtifactModel.cs
@@ -8,8 +8,13 @@ public class ArtifactModel
     public int Quantity { get; set; }
     public double Rating { get; set; }
     public decimal Price { get; set; }
+    public decimal DiscountAmount { get; set; }
     public int VendorId { get; set; }
     public int CategoryId { get; set; }
     public int EraId { get; set; }
     public bool Availability { get; set; }
+    public decimal FinalPrice { get; set; }
 }
+
+
+// Add DiscountAmount and FinalPrice
--- Client/Models/CreateArtifactModel.cs
+++ Client/Models/CreateArtifactModel.cs
@@ -3,32 +3,39 @@
 public class CreateArtifactModel
 {
     [Required(ErrorMessage = "Please provide the artifact's name.")]
-    [StringLength(100, ErrorMessage = "The artifact's name must not exceed 100 characters.")]
+    [StringLength(100, ErrorMessage = "The artifact's name must not exceed 100 characters")]
     public string Name { get; set; }
 
-    [Required(ErrorMessage = "Please provide the artifact's description.")]
-    [StringLength(1000, ErrorMessage = "The artifact's description must not exceed 1000 characters.")]
+    [Required(ErrorMessage = "Please provide the artifact's description")]
+    [StringLength(1000, ErrorMessage = "The artifact's description must not exceed 1000 characters")]
     public string Description { get; set; }
 
     [Display(Name = "Image")]
     public string ImageId { get; set; } = "";
 
-    [Required(ErrorMessage = "Please provide the artifact's quantity.")]
+    [Required(ErrorMessage = "Please provide the artifact's quantity")]
     public int Quantity { get; set; }
 
     [Required(ErrorMessage = "Please provide the artifact's price.")]
-    [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0.")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0")]
     public decimal Price { get; set; }
 
-    [Required(ErrorMessage = "Please provide the vendor ID.")]
+    [Required(ErrorMessage = "Please provide the discount amount")]
+    [Display(Name = "Discount Amount")]
+    public decimal DiscountAmount { get; set; }
+
+    [Required(ErrorMessage = "Please provide the vendor")]
+    [Display(Name = "Vendor")]
     public int VendorId { get; set; }
 
-    [Required(ErrorMessage = "Please provide the category ID.")]
+    [Required(ErrorMessage = "Please provide the category")]
+    [Display(Name = "Category")]
     public int CategoryId { get; set; }
 
-    [Required(ErrorMessage = "Please provide the era ID.")]
+    [Required(ErrorMessage = "Please provide the era")]
+    [Display(Name = "Era")]
     public int EraId { get; set; }
 
-    [Required(ErrorMessage = "Please specify availability.")]
+    [Required(ErrorMessage = "Please specify availability")]
     public bool Availability { get; set; }
 }
